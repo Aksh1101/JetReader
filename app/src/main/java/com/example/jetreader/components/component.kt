8c816113afea665681a42b57bcd0af3d5af33915
Aksh1101 <1101akshat@gmail.com>
2024-06-18 01:33:00 +0530

    day3.0

---
+++ app/src/main/java/com/example/jetreader/components/component.kt
@@ -0,0 +1,84 @@
+package com.example.jetreader.components
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.FocusRequester
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalSoftwareKeyboardController
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import java.security.KeyStore.TrustedCertificateEntry
+
+@Composable
+fun ReaderLogo(modifier: Modifier = Modifier) {
+    Text(
+        text = "Jet Reader!",
+        modifier = modifier.padding(bottom = 16.dp),
+        style = MaterialTheme.typography.displayMedium,
+        color = Color.Red.copy(alpha = 0.5f)
+    )
+}
+
+
+@Composable
+fun EmailInput(modifier: Modifier = Modifier,
+               emailState: MutableState<String>,
+               labelId: String ="Email",
+               enabled:Boolean = true,
+               imeAction: ImeAction = ImeAction.Next,
+               onAction: KeyboardActions = KeyboardActions.Default){
+    InputField(modifier = modifier,
+        valueState = emailState,
+        labelId = labelId,
+        enabled = enabled,
+        keyboardType = KeyboardType.Email,
+        imeAction = imeAction,
+        onAction = onAction)
+}
+
+@Composable
+fun InputField(modifier: Modifier,
+               valueState: MutableState<String>,
+               labelId: String,
+               enabled: Boolean,
+               isSingleLine: Boolean = true,
+               keyboardType: KeyboardType = KeyboardType.Text,
+               imeAction: ImeAction = ImeAction.Next,
+               onAction: KeyboardActions = KeyboardActions.Default) {
+
+    OutlinedTextField(value = valueState.value,
+        onValueChange = { valueState.value = it},
+        label = { Text(text = labelId)},
+        singleLine = isSingleLine,
+        textStyle = TextStyle(fontSize = 18.sp,
+            color = MaterialTheme.colorScheme.onBackground),
+        modifier = modifier
+            .padding(bottom = 10.dp, start = 10.dp, end = 10.dp)
+            .fillMaxWidth(),
+        enabled = enabled,
+        keyboardOptions = KeyboardOptions(keyboardType = keyboardType, imeAction = imeAction),
+        keyboardActions = onAction)
+
+
+}
--- app/src/main/java/com/example/jetreader/screens/login/LoginScreen.kt
+++ app/src/main/java/com/example/jetreader/screens/login/LoginScreen.kt
@@ -1,8 +1,80 @@
 package com.example.jetreader.screens.login
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.FocusRequester
+import androidx.compose.ui.platform.LocalSoftwareKeyboardController
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.navigation.NavController
+import com.example.jetreader.components.EmailInput
+import com.example.jetreader.components.ReaderLogo
 
 @Composable
-fun LoginScreen(){
+fun LoginScreen(navController: NavController){
+    Surface(modifier = Modifier.fillMaxSize()) {
+        Column(verticalArrangement = Arrangement.Top,
+            horizontalAlignment = Alignment.CenterHorizontally){
+            ReaderLogo(modifier = Modifier.padding(top = 100.dp))
+        }
+    }
 
 }
+
+@Preview
+@Composable
+fun UserForm(){
+    val email = rememberSaveable { mutableStateOf("") }
+    val password = rememberSaveable { mutableStateOf("") }
+    val passwordVisibility = rememberSaveable { mutableStateOf(false) }
+    val passwordFocusRequest = FocusRequester.Default
+    val keyboardController = LocalSoftwareKeyboardController.current
+    val valid = remember(email.value, password.value) {
+        email.value.trim().isNotEmpty() && password.value.trim().isNotEmpty()
+    }
+
+    val modifier = Modifier
+        .height(250.dp)
+        .background(MaterialTheme.colorScheme.background)
+        .verticalScroll(rememberScrollState())
+
+    Column(modifier,
+        horizontalAlignment = Alignment.CenterHorizontally){
+        
+        EmailInput(emailState = email,
+            enabled = true,
+            onAction = KeyboardActions {
+                passwordFocusRequest.requestFocus() })
+
+        
+
+
+    }
+
+}
+
